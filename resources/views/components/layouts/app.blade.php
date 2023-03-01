cc288f937b885f1e8c5ec07573094eb418cbcc9d
pikoro1234 <donjuan.jfc@gmail.com>
2023-03-01 23:10:56 +0100

    template layout base html x-slot

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>futbolme - {{ $title ?? 'Titulo Web' }}</title>
+        <meta name="description" content="{{ $metaDescription }}">
+    </head>
+    <body>
+
+        {{-- navbar --}}
+        <x-layouts.navigation />
+        
+        {{-- contenido principal del documento --}}
+        {{ $slot }}
+
+    </body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.app title="Contacto" meta-description="Contacto page" >
+
+    <h1>esto es la pagina contacto</h1>
+
+</x-layouts.app>
