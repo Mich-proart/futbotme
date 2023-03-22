ab8524404b4b82154b4065bee5b8bf089a3f15e7
pikoro1234 <donjuan.jfc@gmail.com>
2023-03-22 22:43:54 +0100

    agregamos funcionalidad a menu cerrar y mostrar con dis y en los otros

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,14 +1,70 @@
 $(document).ready(function () {
 
-    //abrir menu mobile
+    // abrir menu mobile
     $(document).on('click','.content-icon-menu',function(){
         $('.content-navbar').fadeIn()        
         $('.bg-overlay-menu').fadeIn()
     })
 
-    //cerrar menu mobile
+    // cerrar menu mobile
     $(document).on('click','.cerrar-menu, .bg-overlay-menu',function(){
         $('.content-navbar').fadeOut()
         $('.bg-overlay-menu').fadeOut()
+    })
+
+    // abrir y cerrar menu nivel parent
+    $(document).on('click','.content-li-parent',function(){        
+        
+        if($(this).hasClass('girado')){
+
+            $('.ul-children').fadeOut()
+            $('.content-li-parent').removeClass('girado')
+            $('.svg-content').find('svg').css('rotate','0deg')
+
+            $(this).removeClass('girado')
+            $(this).find('.svg-content').find('svg').css('rotate','0deg')
+            $(this).closest('.li-parent').find('.ul-children').fadeOut()
+
+        }else{
+            
+            $('.ul-children').fadeOut()
+            $('.content-li-parent').removeClass('girado')
+            $('.svg-content').find('svg').css('rotate','0deg')
+
+            $(this).addClass('girado')
+            $(this).find('.svg-content').find('svg').css('rotate','90deg')  
+            $(this).closest('.li-parent').find('.ul-children').fadeIn()  
+        }
+    })
+
+    // abrir y cerrar menu nivel childre
+    $(document).on('click','.content-li-children',function(){
+
+        if($(this).hasClass('abierto')){
+
+            $('.ul-children-children').fadeOut()
+            $('.content-li-children').removeClass('abierto')
+            $('.content-icon-sub-sub-menu-manus').removeClass('ocultar-icon')
+            $('.content-icon-sub-sub-menu-minus').addClass('ocultar-icon')
+    
+    
+            $(this).closest('.li-children').find('.ul-children-children').fadeOut()
+            $(this).removeClass('abierto')
+            $(this).find('.content-icon-sub-sub-menu-manus').removeClass('ocultar-icon')
+            $(this).find('.content-icon-sub-sub-menu-minus').addClass('ocultar-icon')
+
+        }else{
+
+            $('.ul-children-children').fadeOut()
+            $('.content-li-children').removeClass('abierto')
+            $('.content-icon-sub-sub-menu-manus').removeClass('ocultar-icon')
+            $('.content-icon-sub-sub-menu-minus').addClass('ocultar-icon')
+    
+    
+            $(this).closest('.li-children').find('.ul-children-children').fadeIn()
+            $(this).addClass('abierto')
+            $(this).find('.content-icon-sub-sub-menu-manus').addClass('ocultar-icon')
+            $(this).find('.content-icon-sub-sub-menu-minus').removeClass('ocultar-icon')
+        }
     })
 })
