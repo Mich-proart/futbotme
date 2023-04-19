8e24919bf558403df8c3ee7b3c4cc8de4657c528
pikoro1234 <donjuan.jfc@gmail.com>
2023-04-19 23:41:33 +0200

    menu desktop dinamic 80

--- resources/views/components/menuphp.blade.php
+++ resources/views/components/menuphp.blade.php
@@ -1,6 +1,149 @@
 <?php $datos = file_get_contents(base_path()."/resources/views/components/menu.json");?>
 
 <?php $menuStatic = array(
+
+    [
+        'textoWeb' => 'Ligas Españolas',        
+        'claveMenu' => 'ligasEspanholas',        
+        'staticSubMenu' => array(
+            [
+                'claveJson' => '',
+                'subMenu' => 'off',
+                'texto' => '1ª División',
+                'enlace' => '/resultados-directo/torneo/primera-division/1/',
+                'bandera' => '',                
+
+            ],
+            [
+                'claveJson' => '',
+                'subMenu' => 'on',
+                'texto' => '2ª División',
+                'enlace' => '',
+                'bandera' => '',
+                'fillStatics' => array(
+                    [
+                        'texto' => '2ª División',
+                        'enlace' => '/resultados-directo/torneo/segunda-division/2/',
+                    ],
+                    [
+                        'texto' => 'Ascenso a 1ª División',
+                        'enlace' => '/resultados-directo/torneo/espana-segunda-division-promocion-de-ascenso/239/',
+                    ],                            
+                )
+            ],
+            [                
+                'claveJson' => 'primeraRFEF',
+                'subMenu' => 'on',
+                'texto' => '1ª Federación',
+                'enlace' => '',
+                'bandera' => '', 
+                'extrafillStatic' => array(
+                    [
+                        'texto' => 'Promociones y Eliminatorias',
+                        'enlace' => '/resultados-directo/torneo/espana-promocion-de-ascenso-1-rfef/3127/',
+                    ],
+                )               
+            ],
+            [                
+                'claveJson' => 'segundaRFEF',
+                'subMenu' => 'on',
+                'texto' => '2ª Federación',
+                'enlace' => '',
+                'bandera' => '',    
+                'extrafillStatic' => array(
+                    [
+                        'texto' => 'Promociones',
+                        'enlace' => '/resultados-directo/torneo/espana-promocion-de-permanencia.2-rfef/3128/',
+                    ],
+                )              
+            ],
+            [                
+                'claveJson' => 'terceraRFEF',
+                'subMenu' => 'on',
+                'texto' => '3ª Federación',
+                'enlace' => '',
+                'bandera' => '',   
+                'extrafillStatic' => array(
+                    [
+                        'texto' => 'Promociones',
+                        'enlace' => '/ascensos-y-descensos/nacional',
+                    ],
+                )                 
+            ],            
+        )
+    ],
+    [
+        'textoWeb' => 'Ligas Autonómicas',        
+        'claveMenuJson' => 'autonomica',        
+        'subMenuJson' => 'on',
+    ],  
+    [
+        'textoWeb' => 'Torneos RFEF - FIFA - UEFA',        
+        'claveMenu' => 'RFEFIFAUEFA',  
+        'staticSubMenu' => array(
+            [                
+                'claveJson' => 'RFEF',
+                'subMenu' => 'on',
+                'texto' => 'RFEF',
+                'enlace' => '',
+                'bandera' => '',                                   
+            ], 
+            [                
+                'claveJson' => 'FIFA',
+                'subMenu' => 'on',
+                'texto' => 'FIFA',
+                'enlace' => '',
+                'bandera' => '',                                   
+            ], 
+            [                
+                'claveJson' => 'UEFA',
+                'subMenu' => 'on',
+                'texto' => 'UEFA',
+                'enlace' => '',
+                'bandera' => '',                                   
+            ], 
+        )
+    ], 
+    [
+        'textoWeb' => 'Fútbol Juvenil España',        
+        'claveMenu' => 'FutbolJuvenilEspanha',  
+        'staticSubMenu' => array(
+            [                
+                'claveJson' => 'juvenil',
+                'subMenu' => 'on',
+                'texto' => 'División de Honor',
+                'enlace' => '',
+                'bandera' => '', 
+                'idJsonArray' => array(286,285,32,27,28,38,39,40,96)                                  
+            ], 
+ 
+        )
+    ], 
+    [
+        'textoWeb' => 'Fútbol Femenino España',        
+        'claveMenu' => 'FutbolFemeninoEspanha',  
+        'staticSubMenu' => array(
+            [                
+                'claveJson' => 'femenino',
+                'subMenu' => 'on',
+                'texto' => '1ª División',
+                'enlace' => '',
+                'bandera' => '', 
+                'idJsonArray' => array(286,285,32,27,28,38,39,40,96)                                  
+            ], 
+ 
+        )
+    ],  
+    [
+        'textoWeb' => 'Fútbol Europa',        
+        'claveMenuJson' => 'europa',        
+        'subMenuJson' => 'on',
+    ], 
+
+
+
+
+
     // [
     //     'textoFront' => 'Ascensos y descensos',
     //     'enlace' => '/ascensos-y-descensos/nacional',
@@ -65,53 +208,70 @@
     //     'claveSubMenu' => 'terceraRFEF',
     //     'bandera' => ''
     // ],
-    [
-        'textoFront' => 'Promoción a 2ª Fed.',
-        'enlace' => '',
-        'subMenu' => 'on',
-        'claveSubMenu' => 'promo2RFEF',
-        'bandera' => ''
-    ],
-    [
-        'textoFront' => 'Autonómicas',
-        'enlace' => '',
-        'subMenu' => 'on',
-        'claveSubMenu' => 'autonomica',
-        'bandera' => ''
-    ],
-    [
-        'textoFront' => 'Torneos',
-        'enlace' => '',
-        'subMenu' => 'on',
-        'claveSubMenu' => 'EspecialTorneos',
-        'bandera' => ''
+    // [
+    //     'textoFront' => 'Ligas Españolas',
+    //     'enlace' => '',
+    //     'subMenu' => 'on',
+    //     'claveSubMenu' => 'primeraRFEF',
+    //     'bandera' => '',
+    //     'staticSubMenu' => array(
+    //         [
+    //             'texto' => '1ª División',
+    //             'enlace' => '/resultados-directo/torneo/primera-division/1/',
+    //             'bandera' => '',
+    //             'subfillKey' => array(
+    //                 [
+                        
+    //                 ]
+    //             )
+    //         ],
+    //         [
+    //             'texto' => '2ª División',
+    //             'enlace' => '/resultados-directo/torneo/segunda-division/2/',
+    //             'bandera' => '',
+    //         ]
+    //     )
+    // ],
+    // [
+    //     'textoFront' => 'Ligas Autonómicas',
+    //     'enlace' => '',
+    //     'subMenu' => 'on',
+    //     'claveSubMenu' => 'autonomica',
+    //     'bandera' => ''
+    // ],
+    // [
+    //     'textoFront' => 'Torneos RFEF - FIFA - UEFA',
+    //     'enlace' => '',
+    //     'subMenu' => 'on',
+    //     'claveSubMenu' => 'EspecialTorneos',
+    //     'bandera' => ''
 
-    ],
-    [
-        'textoFront' => 'Europa',
-        'enlace' => '',
-        'subMenu' => 'on',
-        'claveSubMenu' => 'europa',
-        'bandera' => ''
-    ],
-    [
-        'textoFront' => 'Juvenil',
-        'enlace' => '',
-        'subMenu' => 'on',
-        'claveSubMenu' => 'juvenil',
-        'bandera' => ''
-    ],
-    [
-        'textoFront' => 'Femenino',
-        'enlace' => '',
-        'subMenu' => 'on',
-        'claveSubMenu' => 'femenino',
-        'bandera' => ''
-    ]    
+    // ],
+    // [
+    //     'textoFront' => 'Fútbol Juvenil España',
+    //     'enlace' => '',
+    //     'subMenu' => 'on',
+    //     'claveSubMenu' => 'juvenil',
+    //     'bandera' => ''
+    // ],
+    // [
+    //     'textoFront' => 'Fútbol Femenino España',
+    //     'enlace' => '',
+    //     'subMenu' => 'on',
+    //     'claveSubMenu' => 'femenino',
+    //     'bandera' => ''
+    // ],  
+    // [
+    //     'textoFront' => 'Fútbol Europa',
+    //     'enlace' => '',
+    //     'subMenu' => 'on',
+    //     'claveSubMenu' => 'europa',
+    //     'bandera' => ''
+    // ]  
 );?>
 
 <?php $iObj = json_decode($datos, true);?>
 
 @include('components.components-menu.menudesktop')
 
-@include('components.components-menu.menumobile')
+{{-- @include('components.components-menu.menumobile') --}}
