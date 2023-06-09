d8c5e508e7d41139e862a497276f41e68972ff9a
Michell Arenales <68234112+Mich-proart@users.noreply.github.com>
2023-06-09 18:55:02 +0200

    ajustes de directos por api

--- public/assets/js/apiBetsapi.js
+++ public/assets/js/apiBetsapi.js
@@ -1,36 +1,106 @@
 // hacemos los cambios en partidos directos cambios solo de valores para no afectar el html solo los datos
-const partidosDirecto = (respuesta) =>{
+const partidosDirecto = (respuesta) => {
 
     let fechaActual = new Date();
     let hora = fechaActual.getHours();
-    let minutos = fechaActual.getMinutes();      
+    let minutos = fechaActual.getMinutes();
 
     jQuery('.update-un-minuto').text(hora + ":" + minutos)
 
-    let contentPartidos = jQuery('.content-partidos-live')
+    let contentPartidos = jQuery('#PartidosEnDirecto')
 
     contentPartidos.empty();
 
     for (const iterator of respuesta) {
 
-        let partePartido = iterator.timer.tm > 45 ? 2 : 1
+        //let partePartido = iterator.timer.tm > 45 ? 2 : 1
 
         let obj = `
-        <div class="w-100 mb-5">
-            <div class="div-tiempo-de-juego d-flex">
-                <span class="div-primer-tiempo">${partePartido}<span>T</span></span>
-                <span class="mx-2"></span>
-                <span class="">Minuto <span class="div-minutos-tiempo">${iterator.timer.tm}</span></span>
-            </div> 
-            <span class="equipo-directo" attr-id="">${iterator.home.name} </span><span class="marcador">${iterator.ss}</span><span class="equipo-directo" attr-id=""> ${iterator.away.name}</span>
-        </div>`
+<div class="accordion-item br-0">
+    <h2 class="accordion-header d-flex align-items-center justify-content-between" id="panelsStayOpen-heading-${iterator.id}">
+
+        <div class="d-block py-2 px-4">
+            <span class="icon-resolve-filled">
+                <span class="path1"></span>
+                <span class="path2"></span>
+                <span class="path3"></span>
+            </span>
+        </div>
+
+        <div class="py-2 col-lg-10 col">
+            <i class="bi bi-x-square fs-4 pe-2"></i>
+            <h3 class="d-inline-block color-black fs-01">${iterator.league.name}</h3>
+            <span>-</span>
+            <h4 class="fs-00 d-lg-inline-block">RFEF</h4>
+        </div>
+
+        <button class="accordion-button w-auto py-3 px-4" type="button" data-bs-toggle="collapse"
+            data-bs-target="#panelsStayOpen-collapse-${iterator.id}" aria-expanded="true"
+            aria-controls="panelsStayOpen-collapse-${iterator.id}">
+            <i class="color-gray-medium bi bi-chevron-up fs-3"></i>
+        </button>
+    </h2>
+    <div id="panelsStayOpen-collapse-${iterator.id}" class="accordion-collapse collapse show"
+        aria-labelledby="panelsStayOpen-heading-${iterator.id}">
+        <div class="accordion-body border-bottom">
+            <div class="d-flex aling-items-center justify-content-between ">
+                <div class="col-2 d-flex aling-items-center ">
+                    <div class="d-block py-2 px-1 fs-2">
+                        <span class="icon-resolve-filled">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                            <span class="path3"></span>
+                        </span>
+                    </div>
+                    <div>
+                        <span class="mx-4 hora d-inline-block mt-2 fw-bold">17:30</span>
+                    </div>
+
+                </div>
+                <span class="fs-00 d-grid grid-center-xy jornada_tiempo">${iterator.timer.tm}’</span>
+                <div class="col-2">
+                    <span class="icon-TV d-inline-block fs-1 d-grid grid-center-xy"></span>
+                </div>
+
+            </div>
+            <div
+                class="row mx-0 row-cols-3 g-0 align-items-start justify-content-lg-center justify-content-between my-2">
+                <div class="local">
+                    <i class="bi bi-shield"></i>
+                    <h3 class="d-inline-block color-black fs-01">
+                        ${iterator.home.name} 
+                    </h3>
+                    <div class="goleadores text-end">
+                        <span class="d-block">31´- Amh, 1-0</span>
+                    </div>
+                </div>
+
+                <div class="col-auto marcador px-lg-4 px-3">
+                    <span class="goles-local">${iterator.scores[1].home}</span>
+                    <span class="fs-01 color-red">-</span>
+                    <span class="goles-visitante">${iterator.scores[1].away}</span>
+                </div>
+
+                <div class="visitante">
+                    <i class="bi bi-shield"></i>
+                    <h3 class="d-inline-block color-black fs-01">
+                        ${iterator.away.name}
+                    </h3>
+                    <div class="goleadores">
+                        <span class="d-block">35´- Iñaki Williams (pen.), 1-1</span>
+                        <span class="d-block">45´- Iñaki Williams, 1-2</span>
+                        <span class="d-block">90+5´- Iñaki Williams, 1-2</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>`
 
         jQuery(contentPartidos).append(obj);
 
-        console.log(iterator)        
-        console.log(iterator.home.name)
-        console.log(iterator.timer)            
-        console.log(iterator.timer.tm)            
+        console.log(iterator)
     }
 }
 
@@ -39,7 +109,7 @@ const partidosDirecto = (respuesta) =>{
 * funcion generica para realizar las peticiones de api
 * para reutilizar la funcion la dividimos con switch 
 */
-const hacerPeticion = (tipoPeticion) =>{
+const hacerPeticion = (tipoPeticion) => {
 
     let xmlhttp, urlPeticion;
 
@@ -49,23 +119,23 @@ const hacerPeticion = (tipoPeticion) =>{
         case 'partidosDirecto':
 
             urlPeticion = `${urlBase}api/bestApi`
-            
-        break;
+
+            break;
     }
 
     // Verda código para navegadores modernos
     // False código para navegadores antiguos (IE6, IE5)
     window.XMLHttpRequest ? xmlhttp = new XMLHttpRequest() : xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
-    
+
     // Función de callback para manejar la respuesta de la API
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
 
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 
             // La respuesta se ha recibido correctamente
             let respuesta = xmlhttp.responseText;
 
-            respuesta = JSON.parse(respuesta)            
+            respuesta = JSON.parse(respuesta)
 
             // validamos el tipo de peticion para ejecutar una u otra funcion para devolver distintas respuestas con una misma funcion ajax
             switch (tipoPeticion) {
@@ -73,18 +143,18 @@ const hacerPeticion = (tipoPeticion) =>{
                 case 'partidosDirecto':
 
                     partidosDirecto(respuesta)
-            
-                break;
-            }          
+
+                    break;
+            }
         }
-    };   
-    
+    };
+
     // Realizar la petición a la API (en este caso, se utiliza la API de ejemplo 'https://ejemplo.com/api')
     xmlhttp.open("GET", urlPeticion, true);
 
     // Configura las cabeceras personalizadas
     xmlhttp.setRequestHeader("X-CSRF-TOKEN", csrfToken);
-    
+
 
     xmlhttp.send();
 }
@@ -103,7 +173,7 @@ $(document).ready(function () {
     setInterval(() => {
 
         hacerPeticion('partidosDirecto')
-        
+
     }, 60000000);
 
     /* 
