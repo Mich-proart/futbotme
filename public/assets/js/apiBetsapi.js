0799b85f237796170e8b2688576e5756f03c5ed8
Michell Arenales <68234112+Mich-proart@users.noreply.github.com>
2023-06-09 19:15:17 +0200

    ajustes de directos identado

--- public/assets/js/apiBetsapi.js
+++ public/assets/js/apiBetsapi.js
@@ -16,83 +16,84 @@ const partidosDirecto = (respuesta) => {
         //let partePartido = iterator.timer.tm > 45 ? 2 : 1
 
         let obj = `
-<div class="accordion-item br-0">
-    <h2 class="accordion-header d-flex align-items-center justify-content-between" id="panelsStayOpen-heading-${iterator.id}">
+        <div class="accordion-item br-0">
+            <h2 class="accordion-header d-flex align-items-center justify-content-between" id="panelsStayOpen-heading-${iterator.id}">
 
-        <div class="d-block py-2 px-4">
-            <span class="icon-resolve-filled">
-                <span class="path1"></span>
-                <span class="path2"></span>
-                <span class="path3"></span>
-            </span>
-        </div>
+                <div class="d-block py-2 px-4">
+                    <span class="icon-resolve-filled">
+                        <span class="path1"></span>
+                        <span class="path2"></span>
+                        <span class="path3"></span>
+                    </span>
+                </div>
 
-        <div class="py-2 col-lg-10 col">
-            <i class="bi bi-x-square fs-4 pe-2"></i>
-            <h3 class="d-inline-block color-black fs-01">${iterator.league.name}</h3>
-            <span>-</span>
-            <h4 class="fs-00 d-lg-inline-block">RFEF</h4>
-        </div>
+                <div class="py-2 col-lg-10 col">
+                    <i class="bi bi-x-square fs-4 pe-2"></i>
+                    <h3 class="d-inline-block color-black fs-01">${iterator.league.name}</h3>
+                    <span>-</span>
+                    <h4 class="fs-00 d-lg-inline-block">RFEF</h4>
+                </div>
 
-        <button class="accordion-button w-auto py-3 px-4" type="button" data-bs-toggle="collapse"
-            data-bs-target="#panelsStayOpen-collapse-${iterator.id}" aria-expanded="true"
-            aria-controls="panelsStayOpen-collapse-${iterator.id}">
-            <i class="color-gray-medium bi bi-chevron-up fs-3"></i>
-        </button>
-    </h2>
-    <div id="panelsStayOpen-collapse-${iterator.id}" class="accordion-collapse collapse show"
-        aria-labelledby="panelsStayOpen-heading-${iterator.id}">
-        <div class="accordion-body border-bottom">
-            <div class="d-flex aling-items-center justify-content-between ">
-                <div class="col-2 d-flex aling-items-center ">
-                    <div class="d-block py-2 px-1 fs-2">
-                        <span class="icon-resolve-filled">
-                            <span class="path1"></span>
-                            <span class="path2"></span>
-                            <span class="path3"></span>
-                        </span>
+                <button class="accordion-button w-auto py-3 px-4" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapse-${iterator.id}" aria-expanded="true"
+                    aria-controls="panelsStayOpen-collapse-${iterator.id}">
+                    <i class="color-gray-medium bi bi-chevron-up fs-3"></i>
+                </button>
+            </h2>
+            
+            <div id="panelsStayOpen-collapse-${iterator.id}" class="accordion-collapse collapse show"
+                aria-labelledby="panelsStayOpen-heading-${iterator.id}">
+                <div class="accordion-body border-bottom">
+                    <div class="d-flex aling-items-center justify-content-between ">
+                        <div class="col-2 d-flex aling-items-center ">
+                            <div class="d-block py-2 px-1 fs-2">
+                                <span class="icon-resolve-filled">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                    <span class="path3"></span>
+                                </span>
+                            </div>
+                            <div>
+                                <span class="mx-4 hora d-inline-block mt-2 fw-bold">17:30</span>
+                            </div>
+
+                        </div>
+                        <span class="fs-00 d-grid grid-center-xy jornada_tiempo">${iterator.timer.tm}’</span>
+                        <div class="col-2">
+                            <span class="icon-TV d-inline-block fs-1 d-grid grid-center-xy"></span>
+                        </div>
+
                     </div>
-                    <div>
-                        <span class="mx-4 hora d-inline-block mt-2 fw-bold">17:30</span>
+                    <div
+                        class="row mx-0 row-cols-3 g-0 align-items-start justify-content-lg-center justify-content-between my-2">
+                        <div class="local">
+                            <img src="https://assets.b365api.com/images/team/s/${iterator.home.image_id}.png" class="logo_s img-fluid">
+                            <h3 class="d-inline-block color-black fs-01">
+                                ${iterator.home.name} 
+                            </h3>
+                            <div class="goleadores text-end">
+                            </div>
+                        </div>
+
+                        <div class="col-auto marcador px-lg-4 px-3">
+                            <span class="goles-local">${iterator.scores[1].home}</span>
+                            <span class="fs-01 color-red">-</span>
+                            <span class="goles-visitante">${iterator.scores[1].away}</span>
+                        </div>
+
+                        <div class="visitante">
+                            <img src="https://assets.b365api.com/images/team/s/${iterator.away.image_id}.png" class="logo_s img-fluid">
+                            <h3 class="d-inline-block color-black fs-01">
+                                ${iterator.away.name}
+                            </h3>
+                            <div class="goleadores">
+                            </div>
+                        </div>
                     </div>
 
                 </div>
-                <span class="fs-00 d-grid grid-center-xy jornada_tiempo">${iterator.timer.tm}’</span>
-                <div class="col-2">
-                    <span class="icon-TV d-inline-block fs-1 d-grid grid-center-xy"></span>
-                </div>
-
             </div>
-            <div
-                class="row mx-0 row-cols-3 g-0 align-items-start justify-content-lg-center justify-content-between my-2">
-                <div class="local">
-                    <img src="https://assets.b365api.com/images/team/s/${iterator.home.image_id}.png" class="logo_s img-fluid">
-                    <h3 class="d-inline-block color-black fs-01">
-                        ${iterator.home.name} 
-                    </h3>
-                    <div class="goleadores text-end">
-                    </div>
-                </div>
-
-                <div class="col-auto marcador px-lg-4 px-3">
-                    <span class="goles-local">${iterator.scores[1].home}</span>
-                    <span class="fs-01 color-red">-</span>
-                    <span class="goles-visitante">${iterator.scores[1].away}</span>
-                </div>
-
-                <div class="visitante">
-                    <img src="https://assets.b365api.com/images/team/s/${iterator.away.image_id}.png" class="logo_s img-fluid">
-                    <h3 class="d-inline-block color-black fs-01">
-                        ${iterator.away.name}
-                    </h3>
-                    <div class="goleadores">
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</div>`
+        </div>`
 
         jQuery(contentPartidos).append(obj);
 
