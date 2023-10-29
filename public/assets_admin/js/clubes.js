84d703f21d8bb92d83759608e511a641c34fef57
pikoro1234 <donjuan.jfc@gmail.com>
2023-10-29 12:28:18 +0100

    estructura y show clubes por pais completo

--- public/assets_admin/js/clubes.js
+++ public/assets_admin/js/clubes.js
@@ -3,28 +3,36 @@
 
     // seleccionamos el pais para obtener los clubes
     $(".disparador-paises").on("change", function (e) {
-        const formData = {
-            idPais: this.value,
-        };
-        jQuery.ajax({
-            url: `${urlBase}admin-panel/pais-club/`,
-            type: "POST",
-            data: {
-                formData,
-            },
-            headers: {
-                "X-CSRF-TOKEN": csrfToken,
-            },
-            beforeSend: function () {
-                $(".spiner-clubes").fadeIn();
-            },
-            success: function (response) {
-                console.log(response);
-                let result = JSON.parse(response);
-            },
-            complete: function () {
-                $(".spiner-clubes").fadeOut();
-            },
-        });
+        if (this.value != 0) {
+            const formData = {
+                idPais: this.value,
+            };
+            jQuery.ajax({
+                url: `${urlBase}admin-panel/pais-club/`,
+                type: "POST",
+                data: {
+                    formData,
+                },
+                headers: {
+                    "X-CSRF-TOKEN": csrfToken,
+                },
+                beforeSend: function () {
+                    $(".spiner-clubes").fadeIn()
+                    $('.placeholder-fantasma-clubes').remove()
+                    $('.listado-club-por-pais').html('')
+                },
+                success: function (response) {
+                    console.log(response);
+                    let result = JSON.parse(response)
+                    for (const iterator of result) {
+                        $('.listado-club-por-pais').append(
+                        `<li class="list-group-item" style="cursor:pointer;">${iterator.nombre}</li>`);
+                    }
+                },
+                complete: function () {
+                    $(".spiner-clubes").fadeOut();
+                },
+            });
+        }
     });
 })(window.jQuery);
--- resources/views/admin/jugador.blade.php
+++ resources/views/admin/jugador.blade.php
@@ -1,11 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="row mt-5">
-    <div class="col-12">
-        <h1>Aqui mostraremos todos los jugadores</h1>
-    </div><!-- end col-->
-</div> <!-- end row-->
-
+    <div class="row mt-5">
+        <div class="col-12">
+            <h1>Aqui mostraremos todos los jugadores</h1>
+        </div><!-- end col-->
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="header-title">Clubes</h4>
+                    <table id="fixed-columns-datatable" class="table table-striped nowrap row-border order-column w-100">
+                        <thead>
+                            <tr>
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>Position</th>
+                                <th>Office</th>
+                                <th>Age</th>
+                                <th>Start date</th>
+                                <th>Salary</th>
+                                <th>Extn.</th>
+                                <th>E-mail</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Lael</td>
+                                <td>Greer</td>
+                                <td>Systems Administrator</td>
+                                <td>London</td>
+                                <td>21</td>
+                                <td>2009/02/27</td>
+                                <td>$103,500</td>
+                                <td>6733</td>
+                                <td>l.greer@datatables.net</td>
+                            </tr>
+                            <tr>
+                                <td>Jonas</td>
+                                <td>Alexander</td>
+                                <td>Developer</td>
+                                <td>San Francisco</td>
+                                <td>30</td>
+                                <td>2010/07/14</td>
+                                <td>$86,500</td>
+                                <td>8196</td>
+                                <td>j.alexander@datatables.net</td>
+                            </tr>
+                            <tr>
+                                <td>Shad</td>
+                                <td>Decker</td>
+                                <td>Regional Director</td>
+                                <td>Edinburgh</td>
+                                <td>51</td>
+                                <td>2008/11/13</td>
+                                <td>$183,000</td>
+                                <td>6373</td>
+                                <td>s.decker@datatables.net</td>
+                            </tr>
+                            <tr>
+                                <td>Michael</td>
+                                <td>Bruce</td>
+                                <td>Javascript Developer</td>
+                                <td>Singapore</td>
+                                <td>29</td>
+                                <td>2011/06/27</td>
+                                <td>$183,000</td>
+                                <td>5384</td>
+                                <td>m.bruce@datatables.net</td>
+                            </tr>
+                            <tr>
+                                <td>Donna</td>
+                                <td>Snider</td>
+                                <td>Customer Support</td>
+                                <td>New York</td>
+                                <td>27</td>
+                                <td>2011/01/25</td>
+                                <td>$112,000</td>
+                                <td>4226</td>
+                                <td>d.snider@datatables.net</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div> <!-- end card body-->
+            </div> <!-- end card -->
+        </div><!-- end col-->
+    </div> <!-- end row-->
 @endsection
