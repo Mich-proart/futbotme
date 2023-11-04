16f95cc7c144b01b17ea23e7412f8938d6faffe9
Michell Arenales <68234112+Mich-proart@users.noreply.github.com>
2023-11-04 22:37:55 +0100

    ajustes de calendario diferencia de zona horaria

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -54,69 +54,6 @@ const is_mobile = () => {
     }
 }
 
-/* 
-const weekdays = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-let date = new Date();
-
-function getCurrentDate(element) {
-    if (element) {
-        element.value = date.toISOString().substring(0, 10);
-    }
-    return date;
-}
-
-function generateCalendar() {
-    getCurrentDate(document.getElementById("date"));
-    changeHeader(date);
-}
-
-function setDate(form) {
-    let newDate = new Date(form.date.value);
-    date = new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate());
-    changeHeader(date);
-    return false;
-}
-
-function changeHeader(dateHeader) {
-    const currentDateElement = document.getElementById("currentDate");
-    if (currentDateElement) {
-        const dayOfWeek = weekdays[dateHeader.getDay()];
-        const day = dateHeader.getDate().toString().padStart(2, '0');
-        const month = (dateHeader.getMonth() + 1).toString().padStart(2, '0');
-        currentDateElement.textContent = `${dayOfWeek}, ${day}/${month}`;
-    }
-}
-
-
-function resetDate() {
-    date = new Date();
-    generateCalendar();
-}
-
-function prevDay() {
-    date = new Date(date.getFullYear(), date.getMonth(), date.getDate() - 1);
-    getCurrentDate(document.getElementById("date"));
-    changeHeader(date);
-}
-
-function nextDay() {
-    date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
-    getCurrentDate(document.getElementById("date"));
-    changeHeader(date);
-}
-
-
-window.onload = function () {
-    generateCalendar();
-    const currentDateElement = document.getElementById("currentDate");
-    if (currentDateElement) {
-        const today = new Date();
-        const dayOfWeek = weekdays[today.getDay()];
-        const day = today.getDate().toString().padStart(2, '0');
-        const month = (today.getMonth() + 1).toString().padStart(2, '0');
-        currentDateElement.textContent = `${dayOfWeek}, ${day}/${month}`;
-    }
-}; */
 
 
 const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiembre", "Octubre", "Noviembre", "Diciembre"];
@@ -196,12 +133,25 @@ function generateCalendar() {
     getCurrentDate(document.getElementById("date"));    
 }
 
-function setDate(form) {
+/* function setDate(form) {
     let newDate = new Date(form.date.value);
     date = new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate());
     changeHeader(date);
     return false;
+} */
+
+function setDate(form) {
+    const newDateInput = new Date(form.date.value); // Fecha ingresada por el usuario
+    const offsetMinutes = newDateInput.getTimezoneOffset(); // Obtén la diferencia de zona horaria en minutos
+
+    // Ajusta la fecha para tener en cuenta la diferencia de zona horaria
+    newDateInput.setMinutes(newDateInput.getMinutes() + offsetMinutes);
+
+    date = new Date(newDateInput.getFullYear(), newDateInput.getMonth(), newDateInput.getDate());
+    changeHeader(date);
+    return false;
 }
+
 
 function changeHeader(dateHeader) {
     const currentDateElement = document.getElementById("currentDate");
