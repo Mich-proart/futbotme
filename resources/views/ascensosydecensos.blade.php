f9fde151eb760b255fdd32bbf9b0e8abcd74bd8a
Michell Arenales <68234112+Mich-proart@users.noreply.github.com>
2023-12-12 22:23:24 +0100

    prueba de for en ascensos

--- resources/views/ascensosydecensos.blade.php
+++ resources/views/ascensosydecensos.blade.php
@@ -1,219 +1,63 @@
 <x-layouts.app title="ASCENSOS, DESCENSOS Y PROMOCIONES" meta-description="ASCENSOS, DESCENSOS Y PROMOCIONES page">
 
     <h1 class="title_page my-lg-5 my-3">ASCENSOS, DESCENSOS Y PROMOCIONES</h1>
-    {{dd($datos)}}
+    {{-- {{dd($datos)}} --}}
+
     <div class="accordion accordion-flush" id="accordionAscensos">
-        <div class="accordion-item mb-3">
-            <h2 class="accordion-header">
-                <button
-                    class="d-flex align-items-center justify-content-between accordion-button bg-brand-green text-white"
-                    type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false"
-                    aria-controls="flush-collapseOne">
-                    <div class="nombre">
-                        <span class="text-decoration-none fs-22px fw-semibold">RFEF</span>
-                    </div>
 
-                    <div class="d-block"><i class="bi bi-chevron-up fs-3"></i></div>
-                </button>
-            </h2>
-            <div id="flush-collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionAscensos">
-                <div class="accordion-body px-0">
-                    <div class="row row-cols-2">
-                        <div class="col">
-                            <h2 class="subtitle_page bg-silver-medium p-4">PRIMERA DIVISIÓN</h2>
-                            <div class="items-equipos mt-4 mb-lg-5 mb-4 px-4">
-                                <h3 class="subtitle_page2">Campeón de liga</h3>
+        @foreach ($data as $categoria => $categorias)
 
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item mb-3 border-0">
-                                        <div class="visitante">
-                                            <div class="escudo d-lg-inline-block d-none"><img
-                                                    src="https://assets.b365api.com/images/team/m/2829.png"
-                                                    class="logo_s img-fluid">
-                                            </div>
-                                            <h3 class="d-block">Real Madrid</h3>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="items-equipos mt-4 mb-lg-5 mb-4 px-4">
-                                <h3 class="subtitle_page2">Liga de campeones</h3>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item mb-3 border-0">
-                                        <div class="visitante">
-                                            <div class="escudo d-lg-inline-block d-none"><img
-                                                    src="https://assets.b365api.com/images/team/m/2829.png"
-                                                    class="logo_s img-fluid">
-                                            </div>
-                                            <h3 class="d-block">Real Madrid</h3>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item mb-3 border-0">
-                                        <div class="visitante">
-                                            <div class="escudo d-lg-inline-block d-none"><img
-                                                    src="https://assets.b365api.com/images/team/m/2817.png"
-                                                    class="logo_s img-fluid">
-                                            </div>
-                                            <h3 class="d-block">FC Barcelona</h3>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item mb-3 border-0">
-                                        <div class="visitante">
-                                            <div class="escudo d-lg-inline-block d-none"><img
-                                                    src="https://assets.b365api.com/images/team/m/2817.png"
-                                                    class="logo_s img-fluid">
-                                            </div>
-                                            <h3 class="d-block">FC Barcelona</h3>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item mb-3 border-0">
-                                        <div class="visitante">
-                                            <div class="escudo d-lg-inline-block d-none"><img
-                                                    src="https://assets.b365api.com/images/team/m/2817.png"
-                                                    class="logo_s img-fluid">
-                                            </div>
-                                            <h3 class="d-block">FC Barcelona</h3>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item mb-3 border-0">
-                                        <div class="visitante">
-                                            <div class="escudo d-lg-inline-block d-none"><img
-                                                    src="https://assets.b365api.com/images/team/m/2817.png"
-                                                    class="logo_s img-fluid">
-                                            </div>
-                                            <h3 class="d-block">FC Barcelona</h3>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
+            <div class="accordion-item mb-3">
+                <h2 class="accordion-header">
+                    <button
+                        class="d-flex align-items-center justify-content-between accordion-button bg-brand-green text-white"
+                        type="button" data-bs-toggle="collapse" data-bs-target="#flush_array_{{ $loop->iteration }}"
+                        aria-expanded="false" aria-controls="flush_array_{{ $loop->iteration }}">
+                        <div class="nombre">
+                            <span class="text-decoration-none fs-22px fw-semibold">{{ $categoria }}</span>
                         </div>
-                        <div class="col">
-                            <h2 class="subtitle_page bg-silver-medium p-4">SEGUNDA DIVISIÓN</h2>
-                            <div class="items-equipos mt-4 mb-lg-5 mb-4 px-4">
-                                <h3 class="subtitle_page2">Ascienden a 1ª División</h3>
 
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item mb-3 border-0">
-                                        <div class="visitante">
-                                            <div class="escudo d-lg-inline-block d-none"><img
-                                                    src="https://assets.b365api.com/images/team/m/2829.png"
-                                                    class="logo_s img-fluid">
-                                            </div>
-                                            <h3 class="d-block">Real Madrid</h3>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item mb-3 border-0">
-                                        <div class="visitante">
-                                            <div class="escudo d-lg-inline-block d-none"><img
-                                                    src="https://assets.b365api.com/images/team/m/2829.png"
-                                                    class="logo_s img-fluid">
-                                            </div>
-                                            <h3 class="d-block">Real Madrid</h3>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item mb-3 border-0">
-                                        <div class="visitante">
-                                            <div class="escudo d-lg-inline-block d-none"><img
-                                                    src="https://assets.b365api.com/images/team/m/2829.png"
-                                                    class="logo_s img-fluid">
-                                            </div>
-                                            <h3 class="d-block">Real Madrid</h3>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="items-equipos mt-4 mb-lg-5 mb-4 px-4">
-                                <h3 class="subtitle_page2">Promocionan a 1ª División</h3>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item mb-3 border-0">
-                                        <div class="visitante">
-                                            <div class="escudo d-lg-inline-block d-none"><img
-                                                    src="https://assets.b365api.com/images/team/m/2829.png"
-                                                    class="logo_s img-fluid">
-                                            </div>
-                                            <h3 class="d-block">Real Madrid</h3>
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item mb-3 border-0">
-                                        <div class="visitante">
-                                            <div class="escudo d-lg-inline-block d-none"><img
-                                                    src="https://assets.b365api.com/images/team/m/2817.png"
-                                                    class="logo_s img-fluid">
-                                            </div>
-                                            <h3 class="d-block">FC Barcelona</h3>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col"></div>
-                        <div class="col"></div>
-                    </div>
+                        <div class="d-block"><i class="bi bi-chevron-up fs-3"></i></div>
+                    </button>
+                </h2>
+                <div id="flush_array_{{ $loop->iteration }}" class="accordion-collapse collapse show"
+                    data-bs-parent="#accordionAscensos">
+                    <div class="accordion-body px-0">
+                        <div class="row row-cols-2">
+                            @foreach ($categorias as $temporada => $temporadas)
+                                <div class="col">
 
-                </div>
-            </div>
-        </div>
-        <div class="accordion-item mb-3">
-            <h2 class="accordion-header">
-                <button
-                    class="d-flex align-items-center justify-content-between accordion-button bg-brand-green text-white"
-                    type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsetwo" aria-expanded="false"
-                    aria-controls="flush-collapsetwo">
-                    <div class="nombre">
-                        <span class="text-decoration-none fs-22px fw-semibold">RFEF</span>
-                    </div>
 
-                    <div class="d-block"><i class="bi bi-chevron-up fs-3"></i></div>
-                </button>
-            </h2>
-            <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionAscensos">
-                <div class="accordion-body bg-white-light px-0">
-                    <div class="d-flex align-items-center item-fichajes">
-                        <img src="https://futbolme.com/static/img/jugadores/jugador1105.jpg" alt=""
-                            class="img-fluid">
-                        <div class="ps-4">
-                            <h4 class="fs-01 fw-semibold color-black">Maxi</h4>
-                            <p class="fs-00 fw-medium">Procedencia: Celta</p>
+                                    <h2 class="subtitle_page bg-silver-medium p-4">{{ $temporada }}</h2>
+                                    @foreach ($temporadas as $id => $item)
+                                        <div class="items-equipos mt-4 mb-lg-5 mb-4 px-4">
+                                            <h3 class="subtitle_page2">{{ $item->nombre }}</h3>
+
+                                            <ul class="list-group list-group-flush">
+                                                <li class="list-group-item mb-3 border-0">
+                                                    <div class="visitante">
+                                                        <div class="escudo d-lg-inline-block d-none">
+                                                            <img src="https://futbolme.com/static/img/estadios/estadi{{ $item->equipo_id }}.png"
+                                                                class="logo_s img-fluid"
+                                                                alt="Escudo de {{ $item->equipo }}">
+                                                        </div>
+                                                        <h3 class="d-block">{{ $item->equipo }}</h3>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                </div>
+
+                            @endforeach
                         </div>
-                    </div>
-                    <div class="d-flex align-items-center item-fichajes">
-                        <img src="https://futbolme.com/static/img/jugadores/jugador1105.jpg" alt=""
-                            class="img-fluid">
-                        <div class="ps-4">
-                            <h4 class="fs-01 fw-semibold color-black">Maxi</h4>
-                            <p class="fs-00 fw-medium">Procedencia: Celta</p>
-                        </div>
+
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="accordion-item mb-3">
-            <h2 class="accordion-header">
-                <button
-                    class="d-flex align-items-center justify-content-between accordion-button bg-brand-green text-white"
-                    type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree"
-                    aria-expanded="false" aria-controls="flush-collapseThree">
-                    <div class="nombre">
-                        <span class="text-decoration-none fs-22px fw-semibold">RFEF</span>
-                    </div>
+        @endforeach
 
-                    <div class="d-block"><i class="bi bi-chevron-up fs-3"></i></div>
-                </button>
-            </h2>
-            <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionAscensos">
-                <div class="accordion-body bg-white-light px-0">
 
-                    <div class="d-flex align-items-center item-fichajes">
-                        <img src="https://futbolme.com/static/img/jugadores/jugador1105.jpg" alt=""
-                            class="img-fluid">
-                        <div class="ps-4">
-                            <h4 class="fs-01 fw-semibold color-black">Maxi</h4>
-                            <p class="fs-00 fw-medium">Procedencia: Celta</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
     </div>
 
 </x-layouts.app>
