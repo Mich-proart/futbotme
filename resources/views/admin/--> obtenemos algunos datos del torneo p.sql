5a7cf62f02fb8aad49860c026c968232cade5d53
pikoro1234 <donjuan.jfc@gmail.com>
2023-12-12 07:59:14 +0100

    sql para pestaña ligas o torneos

---
+++ resources/views/admin/--> obtenemos algunos datos del torneo p.sql
@@ -0,0 +1,143 @@
+--> obtenemos algunos datos del torneo para crear el select
+SELECT 
+t.torneo_id,
+tor.tipo_torneo, 
+tor.betsapi,
+tor.nombre, 
+tor.traduccion,
+tor.sexo, 
+tor.desarrollo, 
+tor.categoria_id, ce.nombre categoria_nombre,
+tor.visible,
+tor.categoria_torneo_id, 
+tor.id_original,
+tor.apifutbol,
+tor.apiRFEFcompeticion,
+tor.apiRFEFgrupo,
+tor.whatsapp,
+tor.whatsapp_url,
+pa.id idPais, 
+pa.nombre nombrePais,
+co.id idComunidad, 
+co.nombre nombreComunidad, 
+CASE WHEN (tor.tipo_torneo=1) THEN (select jornadas from liga where id=tor.id) 
+ELSE 0 END as jornadas,
+CASE WHEN (tor.tipo_torneo=1) THEN (select jornadaActiva from liga where id=tor.id) 
+ELSE (select fase_activa from eliminatorio where id=tor.id)  END as jornadaActiva,
+CASE WHEN (tor.tipo_torneo=1) THEN (select tipoClasificacion from liga where id=tor.id) 
+ELSE 0 END as tipoClasificacion,
+CASE WHEN (tor.tipo_torneo=1) THEN (select tipoPuntuacion from liga where id=tor.id) 
+ELSE 0 END as tipoPuntuacion
+FROM temporada t
+INNER JOIN torneo tor ON t.torneo_id=tor.id
+INNER JOIN pais pa ON tor.pais_id=pa.id
+INNER JOIN comunidad co ON tor.comunidad_id=co.id
+INNER JOIN categoria ce ON tor.categoria_id=ce.id WHERE t.id= 99;
+
+--> obtenemos datos de la jornada activa o refresh al cambiar 
+SELECT 
+ct.id as categoria_id, ct.orden as categoria_orden, 
+ct.nombre as categoria_nombre, 
+tor.id as torneo_id, 
+tor.orden as torneo_orden, 
+tor.nombre as torneo_nombre, 
+tor.division_id, 
+co.id as comunidad_id, 
+co.nombre as comunidad_nombre, 
+pa.id as pais_id, 
+pa.nombre as pais_nombre,
+p.id as partido_id, 
+p.estado_partido, 
+p.temporada_id, 
+p.fecha, 
+p.arbitro_id, 
+p.hora_prevista, 
+p.hora_real, 
+p.goles_local, 
+p.goles_visitante, 
+p.jornada, 
+p.clasificado, 
+p.observaciones, 
+p.estadio, 
+p.partidoAPI, 
+p.comentario, 
+p.equipoLocal_id, 
+ec.nombre as local, 
+ec.nombreCorto ncLocal, 
+ec.slug twLocal, 
+ec.betsapi betsapiL, 
+(select comunidad_id from provincia where id= (select provincia_id from localidad where id=(select localidad_id from club where id=ec.club_id) ) ) comunidad_local, 
+(select nombre from comunidad where id= (select comunidad_id from provincia where id= (select provincia_id from localidad where id=(select localidad_id from club where id=ec.club_id) ) ) ) comunidad_local_nombre, 
+(select pais_id from club where id=ec.club_id) pais_local, 
+(select nombre from pais where id=(select pais_id from club where id=ec.club_id)) pais_local_nombre, 
+p.equipoVisitante_id, 
+ef.nombre visitante, 
+ef.nombreCorto ncVisitante, 
+ef.slug twVisitante, 
+ef.betsapi betsapiV, 
+(select comunidad_id from provincia where id= (select provincia_id from localidad where id=(select localidad_id from club where id=ef.club_id) ) ) comunidad_visitante,
+(select nombre from comunidad where id= (select comunidad_id from provincia where id= (select provincia_id from localidad where id=(select localidad_id from club where id=ef.club_id) ) ) ) comunidad_visitante_nombre,
+(select pais_id from club where id=ef.club_id) pais_visitante, 
+(select nombre from pais where id=(select pais_id from club where id=ef.club_id)) pais_visitante_nombre, 
+p.usuario_id, 
+(select count(id) from medio where partido_id=p.id) medios, 
+p.youtube_id, p.betsapi 
+FROM partido p 
+INNER JOIN equipo ec ON ec.id=p.equipoLocal_id 
+INNER JOIN equipo ef ON ef.id=p.equipoVisitante_id 
+INNER JOIN temporada te ON te.id=p.temporada_id 
+INNER JOIN torneo tor ON tor.id=te.torneo_id 
+INNER JOIN comunidad co ON co.id=tor.comunidad_id 
+INNER JOIN pais pa ON pa.id=tor.pais_id 
+INNER JOIN categoriatorneo ct ON ct.id=tor.categoria_torneo_id 
+WHERE p.temporada_id=99 AND p.jornada=13
+ORDER BY p.fecha, p.hora_prevista
+
+--> obtenemos los equipos de la temporada 
+SELECT 
+te.equipo_id, 
+te.grupo, 
+e.nombre, 
+e.nombreCorto, 
+e.estadio_id, 
+e.betsapi, 
+e.club_id, 
+e.equipacion_id, 
+e.slug, 
+l.id localidad_id, 
+p.id provincia_id, 
+c.id comunidad_id, 
+l.nombre localidad, 
+p.nombre provincia, 
+c.nombre comunidad, 
+cl.pais_id, 
+cl.es_seleccion 
+FROM temporada_equipo te 
+INNER JOIN equipo e ON te.equipo_id=e.id 
+INNER JOIN club cl ON e.club_id=cl.id 
+LEFT JOIN localidad l ON cl.localidad_id=l.id 
+LEFT JOIN provincia p ON l.provincia_id=p.id 
+LEFT JOIN comunidad c ON p.comunidad_id=c.id WHERE te.temporada_id=99 ORDER BY e.nombre;
+
+--> pestaña ascensos 
+SELECT 
+a.id, 
+a.categoria_id, 
+a.division_id, 
+a.nombre, a.orden, 
+ae.equipo_id, 
+ae.ascenso_id, 
+ae.posicion, 
+ae.temporada_id, 
+e.nombre equipo, 
+e.club_id, 
+cat.nombre categoria, 
+t.nombre temporada 
+FROM ascenso a 
+INNER JOIN ascensoequipo ae ON a.id=ae.ascenso_id 
+INNER JOIN equipo e ON e.id=ae.equipo_id 
+INNER JOIN temporada t ON t.id=ae.temporada_id 
+INNER JOIN torneo tor ON tor.id=t.torneo_id 
+INNER JOIN categoriatorneo cat ON cat.id=a.categoria_id ORDER BY a.categoria_id, a.division_id, tor.orden, a.orden, ae.posicion;
+
+
--- resources/views/admin/editarPartido.blade.php
+++ resources/views/admin/editarPartido.blade.php
@@ -72,6 +72,170 @@
             </table>
             <div class="row pb-3">
                 <div class="col-12 mt-2">
+                    http://localhost:8000/admin-panel/torneo/216/5361/188 --> no resolve
+
+                    consulta front ascensos
+                    $consulta = 'SELECT a.id, a.categoria_id, a.division_id, a.nombre, a.orden, ae.equipo_id, ae.ascenso_id, ae.posicion, ae.temporada_id, e.nombre equipo, e.club_id, cat.nombre categoria, t.nombre temporada
+                    FROM ascenso a
+                    INNER JOIN ascensoequipo ae ON a.id=ae.ascenso_id
+                    INNER JOIN equipo e ON e.id=ae.equipo_id
+                    INNER JOIN temporada t ON t.id=ae.temporada_id
+                    INNER JOIN torneo tor ON tor.id=t.torneo_id
+                    INNER JOIN categoriatorneo cat ON cat.id=a.categoria_id
+                    ORDER BY a.categoria_id, a.division_id, tor.orden, a.orden, ae.posicion';
+
+
+
+                    para obtener todos los torneos 
+                    SELECT t.id, t.nombre, te.id as temporada_id FROM torneo t INNER JOIN temporada te ON te.torneo_id=t.id WHERE t.visible>4;
+
+
+                    <br><br><br><br><br><br>
+
+                    <?php        
+                    
+                    $temporada_id = 99;
+
+                    $jornadaActiva = 13;
+
+                    $campos = "p.id, p.partidoAPI,
+                    p.temporada_id,
+                    p.estado_partido, 
+                    p.jornada,
+                    p.apuesta apuesta_partido,
+                    fa.nombre fase,  
+                    p.fecha, 
+                    p.hora_prevista, 
+                    p.hora_real,
+                    p.apuesta,
+                    p.clasificado, 
+                    p.goles_local,
+                    p.goles_visitante,
+                    p.grupo_id, p.betsapi, p.codigo_acta,
+                    ec.nombre local, ec.nombreCorto localCorto, ec.slug twitterLocal, ec.federacion_id federacion_id_l,
+                    p.equipoLocal_id, cec.pais_id pais_local, cef.pais_id pais_visitante, ef.federacion_id federacion_id_v,
+                    ef.nombre visitante, ef.nombreCorto visitanteCorto, ef.slug twitterVisitante,
+                    p.equipoVisitante_id,
+                    p.observaciones, p.comentario, cec.id club_local, cef.id club_visitante,
+                    p.estadio, p.arbitro_id,p.youtube_id, p.acta, tor.apuesta apuesta_torneo, 
+                    tor.tipo_torneo, tor.pais_id idPais, tor.visible, (SELECT count(id) FROM gol WHERE partido_id=p.id) goles,e.id estadio_id, e.nombre estadio_nombre, l.nombre estadio_localidad,
+                    (SELECT GROUP_CONCAT(DISTINCT medio_id SEPARATOR '-') FROM partido_medio WHERE partido_id=p.id) tv,
+                    (SELECT CONCAT(apellidos,',',nombre) FROM arbitro WHERE id=p.arbitro_id) nombre_arbitro";
+                            if (442 != $temporada_id && 231 != $temporada_id) {
+                                $campos .= ",
+                    (select concat(p2.goles_local,',',p2.goles_visitante,',',p2.jornada,',', p2.fecha,',', p2.id,',',
+                                    (SELECT tipo_torneo FROM torneo WHERE id=(SELECT torneo_id FROM temporada WHERE id=p2.temporada_id))
+                        ) FROM partido p2 
+                    WHERE p2.equipoLocal_id=p.equipoVisitante_id AND p2.equipoVisitante_id=p.equipoLocal_id AND p2.temporada_id=p.temporada_id AND p2.estado_partido=1 AND p2.grupo_id IS NULL LIMIT 1) as ida ";
+                            }
+                    
+                            $tabla = 'partido p';
+                    
+                            $union = ' INNER JOIN equipo ec ON p.equipoLocal_id=ec.id';
+                            $union .= ' INNER JOIN equipo ef ON p.equipoVisitante_id=ef.id';
+                            $union .= ' INNER JOIN club cec ON ec.club_id=cec.id';
+                            $union .= ' INNER JOIN club cef ON ef.club_id=cef.id';
+                            $union .= ' INNER JOIN fase fa ON p.jornada=fa.id';
+                            $union .= ' INNER JOIN temporada te ON p.temporada_id=te.id';
+                            $union .= ' INNER JOIN torneo tor ON te.torneo_id=tor.id';
+                            $union .= ' LEFT JOIN estadio e ON e.id=ec.estadio_id';
+                            $union .= ' LEFT JOIN localidad l ON l.id=e.localidad_id';
+                    
+                            if (0 == $jornadaActiva) {
+                                $condicion = ' WHERE p.temporada_id='.$temporada_id;
+                            } else {
+                                if (442 == $temporada_id || 231 == $temporada_id) {
+                                    $condicion = " WHERE p.fecha>'2017-06-27' AND p.temporada_id=".$temporada_id.' AND p.jornada='.$jornadaActiva;
+                                } else {
+                                    $condicion = ' WHERE p.temporada_id='.$temporada_id.' AND p.jornada='.$jornadaActiva;
+                                }
+                            }
+                    
+                            if (442 == $temporada_id || 231 == $temporada_id) {
+                                $orden = ' ORDER BY p.fecha DESC, p.hora_prevista';
+                            } else {
+                                $orden = ' ORDER BY p.jornada, p.grupo_id, p.fecha, p.hora_prevista';
+                            }
+                    
+                            $consulta = 'SELECT '.$campos.' FROM '.$tabla.$union.$condicion.$orden; ?>
+
+
+<?php var_dump($consulta);?>
+<br><br><br><br><br>
+
+<?php 
+$temporada_id = 99;
+$consulta = 'SELECT a.id, a.categoria_id, a.division_id, a.nombre, a.orden, ae.equipo_id, ae.ascenso_id, ae.posicion, ae.temporada_id, e.nombre equipo, e.club_id, cat.nombre categoria, t.nombre temporada
+        FROM ascenso a
+        INNER JOIN ascensoequipo ae ON a.id=ae.ascenso_id
+        INNER JOIN equipo e ON e.id=ae.equipo_id
+        INNER JOIN temporada t ON t.id=ae.temporada_id
+        INNER JOIN torneo tor ON tor.id=t.torneo_id
+        INNER JOIN categoriatorneo cat ON cat.id=a.categoria_id
+        ORDER BY a.categoria_id, a.division_id, tor.orden, a.orden, ae.posicion';
+
+        var_dump($consulta);
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<br><br><br><br>
                     <span class="">Observaciones partido</span>
                     <textarea class="w-100 my-2" name="" rows="10">{{ $datosPartido[0]->observaciones }}</textarea>
                 </div>
