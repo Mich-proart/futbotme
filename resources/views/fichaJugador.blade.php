591b96f507d4913c8a80609af388ea13bb8e37e9
Michell Arenales <68234112+Mich-proart@users.noreply.github.com>
2023-12-13 16:57:42 +0100

    ficcha jugador22

--- resources/views/fichaJugador.blade.php
+++ resources/views/fichaJugador.blade.php
@@ -8,7 +8,7 @@
     ]; ?>
 
     <div class="row mt-lg-5 pt-lg-5 pb-lg-5 mb-lg-5 mt-5">
-        <div class="col-lg-5 col-12">
+        <div class="col-lg-5 col-12 mb-lg-3 mb-4">
             <h1 class="bg-brand-green text-white w-100 py-3 mb-4 px-3 d-lg-none d-block">
                 <strong>{{ $datos[0]->nombreJugador }}</strong></h1>
             <div class="content-img-jugador" style="border-radius: 6px;">
@@ -20,7 +20,7 @@
         <div class="col-lg-7 col-12 mb-lg-0 mb-5">
             <h1 class="bg-brand-green text-white w-100 py-3 mb-4 px-3 d-lg-block d-none">
                 <strong>{{ $datos[0]->nombreJugador }}</strong></h1>
-            <div class="content-datos-ficha-jugador px-3">
+            <div class="content-datos-ficha-jugador px-lg-3">
                 <p class="color-gray-medium fs-4">Apodo: <span
                         class="fw-bold color-black">{{ $datos[0]->apodoJugador }}</span></p>
                 <p class="color-gray-medium fs-4">Nacimiento: <span
@@ -33,8 +33,8 @@
                         class="fw-bold color-black">{{ $datos[0]->alturaJugador }}m</span></p>
                 <p class="color-gray-medium fs-4"><strong>{{ $arrayPosiciones[$datos[0]->posicionJugador] }}</strong>
                 </p>
-                <p class="color-gray-medium fs-4">Equipo Actual: <span
-                        class="underline">{{ $datos[0]->nombreEquipo }}</span></p>
+                <p class="color-gray-medium fs-4">Equipo Actual: <u
+                        class="underline">{{ $datos[0]->nombreEquipo }}</u></p>
             </div>
         </div>
         <div class="col-12 mt-4">
