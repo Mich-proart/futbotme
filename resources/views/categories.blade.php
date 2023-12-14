79979fdc0da3afc7c9dbc16b5380d4a5cba5ce3b
Michell Arenales <68234112+Mich-proart@users.noreply.github.com>
2023-12-14 11:54:42 +0100

    test para jornada activa2

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -79,9 +79,9 @@
                 {{-- JORNADAS --}}
                 <div class="jornadas_categorias mt-3 mb-5">
 
-                    {{dd($JornadaActiva)}}
+                    {{-- {{dd($JornadaActiva)}} --}}
                     {{-- EJEMPLOS OJO CON LAS CLASES DE LOS DIRECTOS, FUTUROS Y FINALIZADOS --}}
-                    <div id="" class="PartidosEnDirecto bg-white p-lg-4 p-3 mb-2">
+                    {{--  <div id="" class="PartidosEnDirecto bg-white p-lg-4 p-3 mb-2">
 
                         <div class="d-flex aling-items-center justify-content-between ">
                             <div class="col-2 d-flex aling-items-center ">
@@ -131,267 +131,65 @@
                             </div>
                         </div>
 
-                    </div>
+                    </div> --}}
+                    @foreach ($JornadaActiva as $partido)
+                        <?php
+                        $horabd = $partido->hora_prevista;
+                        $HoraLimpia = substr($horabd, 0, -3); // Elimina los tres últimos caracteres :00
+                        ?>
+                        <div id="" class="PartidosFuturos bg-white p-lg-4 p-3 mb-2">
+                            <div class="d-flex aling-items-center justify-content-between partido_futuro">
+                                <div class="col d-flex aling-items-center ">
+                                    <div class="d-block py-2 px-1 fs-2">
+                                        <span class="icon-resolve-filled">
+                                            <span class="path1"></span>
+                                            <span class="path2"></span>
+                                            <span class="path3"></span>
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <span
+                                            class="mx-4 hora minitos_partidos d-inline-block fw-semibold px-3 bg-gray-hora text-white">{{ $HoraLimpia }}</span>
+                                    </div>
 
-
-                    <div id="" class="PartidosEnDirecto bg-white p-lg-4 p-3 mb-2">
-
-                        <div class="d-flex aling-items-center justify-content-between ">
-                            <div class="col-2 d-flex aling-items-center ">
-                                <div class="d-block py-2 px-1 fs-2">
-                                    <span class="icon-resolve-filled">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                        <span class="path3"></span>
-                                    </span>
                                 </div>
-                                <div>
-                                    <span
-                                        class="mx-4 hora minitos_partidos d-inline-block fw-semibold px-4 bg-green-minutos text-white">10’</span>
+                                <div class="col"><span
+                                        class="fs-00 d-grid grid-center-xy jornada_tiempo">{{ $partido->jornada }}</span>
                                 </div>
-
-                            </div>
-
-                            <div
-                                class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
-                                <span class="icon-alienacion d-inline-block fs-1"></span>
-                                <span class="icon-ball d-inline-block fs-1"></span>
-                                <span class="icon-TV d-inline-block fs-1"></span>
-                            </div>
-
-                        </div>
-                        <div class="mx-0 partido_alineado my-2">
-                            <div class="local">
-                                <h3 class="d-block">Real Madrid</h3>
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2829.png"
-                                        class="logo_s img-fluid">
-                                </div>
-                            </div>
-
-                            <div class="marcador">
-                                <span class="goles-local">4</span>
-                                <span class="fs-01 color-red">-</span>
-                                <span class="goles-visitante">0</span>
-                            </div>
-
-                            <div class="visitante">
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2817.png"
-                                        class="logo_s img-fluid">
-                                </div>
-                                <h3 class="d-block">FC Barcelona</h3>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div id="" class="PartidosEnDirecto bg-white p-lg-4 p-3 mb-2">
-
-                        <div class="d-flex aling-items-center justify-content-between ">
-                            <div class="col-2 d-flex aling-items-center ">
-                                <div class="d-block py-2 px-1 fs-2">
-                                    <span class="icon-resolve-filled">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                        <span class="path3"></span>
-                                    </span>
-                                </div>
-                                <div>
-                                    <span
-                                        class="mx-4 hora minitos_partidos d-inline-block fw-semibold px-4 bg-green-minutos text-white">10’</span>
-                                </div>
-
-                            </div>
-
-                            <div
-                                class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
-                                <span class="icon-alienacion d-inline-block fs-1"></span>
-                                <span class="icon-ball d-inline-block fs-1"></span>
-                                <span class="icon-TV d-inline-block fs-1"></span>
-                            </div>
-
-                        </div>
-                        <div class="mx-0 partido_alineado my-2">
-                            <div class="local">
-                                <h3 class="d-block">Real Madrid</h3>
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2829.png"
-                                        class="logo_s img-fluid">
-                                </div>
-                            </div>
-
-                            <div class="marcador">
-                                <span class="goles-local">4</span>
-                                <span class="fs-01 color-red">-</span>
-                                <span class="goles-visitante">0</span>
-                            </div>
-
-                            <div class="visitante">
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2817.png"
-                                        class="logo_s img-fluid">
-                                </div>
-                                <h3 class="d-block">FC Barcelona</h3>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div id="" class="PartidosFuturos bg-white p-lg-4 p-3 mb-2">
-                        <div class="d-flex aling-items-center justify-content-between partido_futuro">
-                            <div class="col d-flex aling-items-center ">
-                                <div class="d-block py-2 px-1 fs-2">
-                                    <span class="icon-resolve-filled">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                        <span class="path3"></span>
-                                    </span>
-                                </div>
-                                <div>
-                                    <span
-                                        class="mx-4 hora minitos_partidos d-inline-block fw-semibold px-3 bg-gray-hora text-white">17:30</span>
-                                </div>
-
-                            </div>
-                            <div class="col"><span class="fs-00 d-grid grid-center-xy jornada_tiempo">Jornada
-                                    30</span></div>
-                            <div
-                                class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
-                                {{-- <span class="icon-alienacion d-inline-block fs-1"></span>
+                                <div
+                                    class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
+                                    {{-- <span class="icon-alienacion d-inline-block fs-1"></span>
                                 <span class="icon-ball d-inline-block fs-1"></span> --}}
-                                <span class="icon-TV d-inline-block fs-1"></span>
-                            </div>
-
-                        </div>
-                        <div class="mx-0 partido_alineado my-2">
-                            <div class="local">
-                                <h3 class="d-block">Real Madrid</h3>
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2829.png"
-                                        class="logo_s img-fluid"></div>
-                            </div>
-
-                            <div class="marcador">
-                                <span class="goles-local">-</span>
-                                <span class="fs-01 color-red">-</span>
-                                <span class="goles-visitante">-</span>
-                            </div>
-
-                            <div class="visitante">
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2817.png"
-                                        class="logo_s img-fluid"></div>
-                                <h3 class="d-block">FC Barcelona</h3>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div id="" class="PartidosFuturos bg-white p-lg-4 p-3 mb-2">
-                        <div class="d-flex aling-items-center justify-content-between partido_futuro">
-                            <div class="col d-flex aling-items-center ">
-                                <div class="d-block py-2 px-1 fs-2">
-                                    <span class="icon-resolve-filled">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                        <span class="path3"></span>
-                                    </span>
-                                </div>
-                                <div>
-                                    <span
-                                        class="mx-4 hora minitos_partidos d-inline-block fw-semibold px-3 bg-gray-hora text-white">17:30</span>
+                                    <span class="icon-TV d-inline-block fs-1"></span>
                                 </div>
 
                             </div>
-                            <div class="col"><span class="fs-00 d-grid grid-center-xy jornada_tiempo">Jornada
-                                    30</span></div>
-                            <div
-                                class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
-                                {{-- <span class="icon-alienacion d-inline-block fs-1"></span>
-                                <span class="icon-ball d-inline-block fs-1"></span> --}}
-                                <span class="icon-TV d-inline-block fs-1"></span>
-                            </div>
-
-                        </div>
-                        <div class="mx-0 partido_alineado my-2">
-                            <div class="local">
-                                <h3 class="d-block">Real Madrid</h3>
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2829.png"
-                                        class="logo_s img-fluid"></div>
-                            </div>
-
-                            <div class="marcador">
-                                <span class="goles-local">-</span>
-                                <span class="fs-01 color-red">-</span>
-                                <span class="goles-visitante">-</span>
-                            </div>
-
-                            <div class="visitante">
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2817.png"
-                                        class="logo_s img-fluid"></div>
-                                <h3 class="d-block">FC Barcelona</h3>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div id="" class="PartidosFuturos bg-white p-lg-4 p-3 mb-2 ">
-                        <div class="d-flex aling-items-center justify-content-between partido_finalizado">
-                            <div class="col d-flex aling-items-center ">
-                                <div class="d-block py-2 px-1 fs-2">
-                                    <span class="icon-resolve-filled">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                        <span class="path3"></span>
-                                    </span>
-                                </div>
-                                <div>
-                                    <span
-                                        class="mx-4 hora minitos_partidos d-inline-block fw-semibold px-3 bg-gray-hora text-white">17:30</span>
+                            <div class="mx-0 partido_alineado my-2">
+                                <div class="local">
+                                    <h3 class="d-block">{{ $partido->local }}</h3>
+                                    <div class="escudo d-lg-inline-block d-none"><img
+                                            src="{{ asset('assets/images/img/club/escudo' . $partido->equipoLocal_id) }}.png"
+                                            class="logo_s img-fluid"></div>
                                 </div>
 
-                            </div>
-                            <div class="col"><span class="fs-00 d-grid grid-center-xy jornada_tiempo">Jornada
-                                    30</span></div>
-                            <div
-                                class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
-                                <span class="icon-alienacion d-inline-block fs-1"></span>
-                                <span class="icon-ball d-inline-block fs-1"></span>
-                                <span class="icon-TV d-inline-block fs-1"></span>
-                            </div>
+                                <div class="marcador">
+                                    <span class="goles-local">{{ $partido->goles_local }}</span>
+                                    <span class="fs-01 color-red">-</span>
+                                    <span class="goles-visitante">{{ $partido->goles_visitante }}</span>
+                                </div>
 
-                        </div>
-                        <div class="mx-0 partido_alineado my-2">
-                            <div class="local">
-                                <h3 class="d-block">Real Madrid</h3>
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2829.png"
-                                        class="logo_s img-fluid"></div>
-                                <div class="goleadores">
-                                    <span class="d-block">0-1, Amath - 21´</span>
-                                    <span class="d-block">0-2, Joan - 30´</span>
-                                    <span class="d-block">1-3, Ronaldo - 55´</span>
+                                <div class="visitante">
+                                    <div class="escudo d-lg-inline-block d-none"><img
+                                            src="{{ asset('assets/images/img/club/escudo' . $partido->equipoVisitante_id) }}.png"
+                                            class="logo_s img-fluid"></div>
+                                    <h3 class="d-block">{{ $partido->visitante }}</h3>
                                 </div>
                             </div>
 
-                            <div class="marcador">
-                                <span class="goles-local">10</span>
-                                <span class="fs-01 color-red">-</span>
-                                <span class="goles-visitante">16</span>
-                            </div>
-
-                            <div class="visitante">
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2817.png"
-                                        class="logo_s img-fluid"></div>
-                                <h3 class="d-block">FC Barcelona</h3>
-                            </div>
                         </div>
+                    @endforeach
 
-                    </div>
+
 
                 </div>
 
