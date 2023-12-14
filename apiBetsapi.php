5b81e1b1460ee83273141f3c7dbf0fefa40c0dfd
Michell Arenales <68234112+Mich-proart@users.noreply.github.com>
2023-12-14 17:18:55 +0100

    test para imprimir array de alineaciones y eventos 1

---
+++ apiBetsapi.php
@@ -0,0 +1,22 @@
+<?php
+// URL del endpoint
+$url = 'https://api.b365api.com/v1/event/lineup?token=153716-4djEyj4e6JZVou&LNG_ID=3&event_id=' . $_POST['id'];
+
+// Inicializar la solicitud cURL
+$ch = curl_init();
+
+// Establecer la URL
+curl_setopt($ch, CURLOPT_URL, $url);
+
+// Ejecutar la solicitud
+$response = curl_exec($ch);
+
+// Verificar si hubo algún error en la solicitud
+if (curl_errno($ch)) {
+    echo 'Error en la solicitud: ' . curl_error($ch);
+} else {
+    return json_encode($response);
+}
+
+// Cerrar la conexión cURL
+curl_close($ch);
