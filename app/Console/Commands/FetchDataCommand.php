a9833fb52e8ca488afc396c1ce3429d8d3db1394
pikoro1234 <donjuan.jfc@gmail.com>
2023-12-19 00:15:27 +0100

    ajuste en consulta de poner partidos en finalizados

--- app/Console/Commands/FetchDataCommand.php
+++ app/Console/Commands/FetchDataCommand.php
@@ -38,17 +38,27 @@ class FetchDataCommand extends Command
         ->get();
         // 6809273
         foreach ($dataDb as $item) {
-            $url = 'https://api.b365api.com/v1/event/view?token=153716-4djEyj4e6JZVou&event_id='.$item->betsapi;
+            //https://api.b365api.com/v1/event/view?token=153716-4djEyj4e6JZVou&event_id=6809275&LNG_ID=3
+            $url = 'https://api.b365api.com/v1/event/view?token=153716-4djEyj4e6JZVou&event_id='.$item->betsapi.'&LNG_ID=3';
             $response = Http::get($url);
             if ($response->ok()) {
                 $data = $response->json();
                 foreach ($data['results'] as $key => $value) {
                     if(intval($value['id']) === intval($item->betsapi)){
-                        var_dump($value['id']);
+                        $goles = $value['ss'];
+                        if ($goles === null) {
+                            $goles = "0-0";
+                        }
+                        $goles = explode("-", $goles);
+                        $goles_local = $goles[0];
+                        $goles_visitante = $goles[1];
+                        var_dump($value['id'].' - events mayor > 0 '.count($value['events']));
                         $updateDataPartido = DB::table('partido')
                         ->where('betsapi', $value['id'])
                         ->update([
-                            'estado_partido' => $value['time_status'],
+                            'estado_partido' => count($value['events']) > 0 ? 1 : $value['time_status'],
+                            'goles_local' => $goles_local,
+                            'goles_visitante' => $goles_visitante
                         ]);
                     }
                 }
