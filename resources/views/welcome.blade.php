b4216020b164bbb041ab7cc70e294fda32886357
Michell Arenales <68234112+Mich-proart@users.noreply.github.com>
2023-12-22 18:01:12 +0100

    ajuste de eventos2456

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -378,7 +378,31 @@
                             </div>
 
                         </div>
+                        <div class="d-none content-eventos de-fila-partido">
+                            <div class="d-flex align-items-center justify-content-between w-100">
+                                <h3 class="d-block">Eventos</h3>
+                                <span class="cerrar-eventos display-4" style="cursor: pointer;">&times;</span>
+                            </div>
+                            <ul class="list-group lista-eventos text-left"></ul>
+                        </div>
 
+
+                        <div class="d-none content-alineaciones">
+                            <div class="d-flex align-items-center justify-content-between w-100">
+                                <h3 class="d-block">Alineaciones</h3>
+                                <span class="cerrar-alineacion display-4" style="cursor: pointer;">&times;</span>
+                            </div>
+                            <div class="align-items-start flex-wrap d-flex">
+                                <div class="left-content-alin pr-lg-3 w-50 text-left">
+                                    <h4 class="title-alineacion title-alineacion-locales">Local</h4>
+                                    <ul class="list-unstyled listado-locales"></ul>
+                                </div>
+                                <div class="rigth-content-alin w-50 text-left">
+                                    <h4 class="title-alineacion title-alineacion-visitantes">Visitante</h4>
+                                    <ul class="list-unstyled listado-visitantes"></ul>
+                                </div>
+                            </div>
+                        </div>
 
                         <?php } ?>
                     </div>
@@ -920,28 +944,28 @@
 
     <script>
         /* function terravison() {
-                    $.ajax({
-                        url: '{{ route('leer-fichero') }}',
-                        method: 'POST',
-                        data: {
-                            //nuevoJActiva: nuevoJActiva,
-                            //id: id,
-                            _token: '{{ csrf_token() }}',
-                        },
-                        success: function(response) {
-                            // Actualizar el front-end con la nueva vista parcial
-                            console.log(response);
-                            $('#PartidosEnDirecto').html(response);
+                        $.ajax({
+                            url: '{{ route('leer-fichero') }}',
+                            method: 'POST',
+                            data: {
+                                //nuevoJActiva: nuevoJActiva,
+                                //id: id,
+                                _token: '{{ csrf_token() }}',
+                            },
+                            success: function(response) {
+                                // Actualizar el front-end con la nueva vista parcial
+                                console.log(response);
+                                $('#PartidosEnDirecto').html(response);
 
-                        },
-                        error: function(jqXHR, textStatus, errorThrown) {
-                            console.error("Error en la solicitud Ajax:", textStatus, errorThrown);
+                            },
+                            error: function(jqXHR, textStatus, errorThrown) {
+                                console.error("Error en la solicitud Ajax:", textStatus, errorThrown);
 
-                            // Mostrar detalles del error en la consola
-                            console.log(jqXHR);
-                        }
-                    });
-                } */
+                                // Mostrar detalles del error en la consola
+                                console.log(jqXHR);
+                            }
+                        });
+                    } */
 
 
         function terravison() {
