ca2b8199a5e327d8b1ce95211e61a14079cade3e
Michell Arenales <68234112+Mich-proart@users.noreply.github.com>
2024-01-07 14:16:34 +0100

    print de torneo2

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.app title="Categorias dinamicas" meta-description="Categorias page">
-    {{ dd($torneo_Liga) }}
+    {{-- {{ dd($torneo_Liga) }} --}}
 
     {{-- <p>Torneo ID: {{ $torneo_Liga->torneo_id }}</p>
     <p>Tipo Torneo: {{ $torneo_Liga->tipo_torneo }}</p> --}}
@@ -21,6 +21,7 @@
             partido, que materializan 128 victorias locales, 100 empates y 78 victorias visitantes.</p>
     </section>
     <section>
+        <?php if ($torneo_Liga->tipo_torneo == 1) { ?>
         <ul class="nav nav-pills mb-3 justify-content-lg-start justify-content-center pb-2" id="pills_tab_cat"
             role="tablist">
             <li class="nav-item" role="presentation">
@@ -69,8 +70,8 @@
 
                     <label for="number" class="fs-4">JORNADA</label>
                     {{-- <input id="number" type="number" pattern="[0-9]*" name="value"
-                        value="{{ $torneo_Liga->jornadaActiva }}" min="1" max="{{ $torneo_Liga->jornadas }}"
-                        step="1" maxlength="3" class="text-white fs-4"> --}}
+                            value="{{ $torneo_Liga->jornadaActiva }}" min="1" max="{{ $torneo_Liga->jornadas }}"
+                            step="1" maxlength="3" class="text-white fs-4"> --}}
 
                     <select name="value" id="number">
                         @foreach (range(1, $torneo_Liga->jornadas) as $jornada)
@@ -90,55 +91,55 @@
                     {{-- EJEMPLOS OJO CON LAS CLASES DE LOS DIRECTOS, FUTUROS Y FINALIZADOS --}}
                     {{--  <div id="" class="PartidosEnDirecto bg-white p-lg-4 p-3 mb-2">
 
-                        <div class="d-flex aling-items-center justify-content-between ">
-                            <div class="col-2 d-flex aling-items-center ">
-                                <div class="d-block py-2 px-1 fs-2">
-                                    <span class="icon-resolve-filled">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                        <span class="path3"></span>
-                                    </span>
+                            <div class="d-flex aling-items-center justify-content-between ">
+                                <div class="col-2 d-flex aling-items-center ">
+                                    <div class="d-block py-2 px-1 fs-2">
+                                        <span class="icon-resolve-filled">
+                                            <span class="path1"></span>
+                                            <span class="path2"></span>
+                                            <span class="path3"></span>
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <span
+                                            class="mx-4 hora minitos_partidos d-inline-block fw-semibold px-4 bg-red-minutos text-white">74’</span>
+                                    </div>
+
                                 </div>
-                                <div>
-                                    <span
-                                        class="mx-4 hora minitos_partidos d-inline-block fw-semibold px-4 bg-red-minutos text-white">74’</span>
+
+                                <div
+                                    class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
+                                    <span class="icon-alienacion d-inline-block fs-1"></span>
+                                    <span class="icon-ball d-inline-block fs-1"></span>
+                                    <span class="icon-TV d-inline-block fs-1"></span>
                                 </div>
 
                             </div>
+                            <div class="mx-0 partido_alineado my-2">
+                                <div class="local">
+                                    <h3 class="d-block">Real Madrid</h3>
+                                    <div class="escudo d-lg-inline-block d-none"><img
+                                            src="https://assets.b365api.com/images/team/m/2829.png"
+                                            class="logo_s img-fluid">
+                                    </div>
+                                </div>
 
-                            <div
-                                class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
-                                <span class="icon-alienacion d-inline-block fs-1"></span>
-                                <span class="icon-ball d-inline-block fs-1"></span>
-                                <span class="icon-TV d-inline-block fs-1"></span>
-                            </div>
+                                <div class="marcador">
+                                    <span class="goles-local">4</span>
+                                    <span class="fs-01 color-red">-</span>
+                                    <span class="goles-visitante">0</span>
+                                </div>
 
-                        </div>
-                        <div class="mx-0 partido_alineado my-2">
-                            <div class="local">
-                                <h3 class="d-block">Real Madrid</h3>
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2829.png"
-                                        class="logo_s img-fluid">
+                                <div class="visitante">
+                                    <div class="escudo d-lg-inline-block d-none"><img
+                                            src="https://assets.b365api.com/images/team/m/2817.png"
+                                            class="logo_s img-fluid">
+                                    </div>
+                                    <h3 class="d-block">FC Barcelona</h3>
                                 </div>
                             </div>
 
-                            <div class="marcador">
-                                <span class="goles-local">4</span>
-                                <span class="fs-01 color-red">-</span>
-                                <span class="goles-visitante">0</span>
-                            </div>
-
-                            <div class="visitante">
-                                <div class="escudo d-lg-inline-block d-none"><img
-                                        src="https://assets.b365api.com/images/team/m/2817.png"
-                                        class="logo_s img-fluid">
-                                </div>
-                                <h3 class="d-block">FC Barcelona</h3>
-                            </div>
-                        </div>
-
-                    </div> --}}
+                        </div> --}}
                     @foreach ($JornadaActiva as $partido)
                         <?php
                         $horabd = $partido->hora_prevista;
@@ -178,7 +179,7 @@
                                 <div
                                     class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
                                     {{-- <span class="icon-alienacion d-inline-block fs-1"></span>
-                                <span class="icon-ball d-inline-block fs-1"></span> --}}
+                                    <span class="icon-ball d-inline-block fs-1"></span> --}}
                                     <span class="icon-TV d-inline-block fs-1"></span>
                                 </div>
 
@@ -703,7 +704,7 @@
                                 <div
                                     class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
                                     {{-- <span class="icon-alienacion d-inline-block fs-1"></span>
-                                    <span class="icon-ball d-inline-block fs-1"></span> --}}
+                                        <span class="icon-ball d-inline-block fs-1"></span> --}}
                                     <span class="icon-TV d-inline-block fs-1"></span>
                                 </div>
 
@@ -753,7 +754,7 @@
                                 <div
                                     class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
                                     {{-- <span class="icon-alienacion d-inline-block fs-1"></span>
-                                    <span class="icon-ball d-inline-block fs-1"></span> --}}
+                                        <span class="icon-ball d-inline-block fs-1"></span> --}}
                                     <span class="icon-TV d-inline-block fs-1"></span>
                                 </div>
 
@@ -1029,11 +1030,116 @@
                 </div>
             </div>
             {{-- <div class="tab-pane fade" id="pills-ascensos-descensos" role="tabpanel"
-                aria-labelledby="pills-ascensos-descensos-tab">...</div>
-            <div class="tab-pane fade" id="pills-golaverage" role="tabpanel" aria-labelledby="pills-golaverage-tab">
-                ...
-            </div> --}}
+                    aria-labelledby="pills-ascensos-descensos-tab">...</div>
+                <div class="tab-pane fade" id="pills-golaverage" role="tabpanel" aria-labelledby="pills-golaverage-tab">
+                    ...
+                </div> --}}
         </div>
+        <?php }elseif ($torneo_Liga->tipo_torneo == 2) { ?>
+        <div class="jornadas_categorias mt-3 mb-5" id="jornadas_categorias">
+            @foreach ($JornadaActiva as $partido)
+                <?php
+                $horabd = $partido->hora_prevista;
+                $HoraLimpia = substr($horabd, 0, -3); // Elimina los tres últimos caracteres :00
+                $observaciones = $partido->observaciones;
+                
+                // Buscar la posición de *A y *B en el string
+                $posicionA = strpos($observaciones, '*A');
+                $posicionB = strpos($observaciones, '*B');
+                
+                // Extraer la primera variable desde *A hasta justo antes de *B
+                $goles_local = substr($observaciones, $posicionA + 2, $posicionB - ($posicionA + 2));
+                
+                // Extraer la segunda variable desde *B hasta el final del observaciones
+                $goles_visitante = substr($observaciones, $posicionB + 2);
+                ?>
+                <div id="" class="PartidosFuturos bg-white p-lg-4 p-3 mb-2">
+                    <div class="d-flex aling-items-center justify-content-between partido_futuro">
+                        <div class="col d-flex aling-items-center ">
+                            <div class="d-block py-2 px-1 fs-2">
+                                <span class="icon-resolve-filled">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                    <span class="path3"></span>
+                                </span>
+                            </div>
+                            <div>
+                                <span
+                                    class="mx-4 hora minitos_partidos d-inline-block fw-semibold px-3 bg-gray-hora text-white">{{ $HoraLimpia }}</span>
+                            </div>
+
+                        </div>
+                        <div class="col">
+                            <span class="fs-00 d-grid grid-center-xy jornada_tiempo">Jornada
+                                {{ $partido->jornada }}</span>
+                        </div>
+                        <div class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
+                            {{-- <span class="icon-alienacion d-inline-block fs-1"></span>
+                                    <span class="icon-ball d-inline-block fs-1"></span> --}}
+                            <span class="icon-TV d-inline-block fs-1"></span>
+                        </div>
+
+                    </div>
+                    <div class="mx-0 partido_alineado my-2">
+                        <div class="local">
+                            <h3 class="d-block">{{ $partido->local }}</h3>
+                            <div class="escudo d-lg-inline-block d-none">
+                                {{-- <img src="{{ asset('assets/images/img/club/escudo' . $partido->equipoLocal_id) }}.png" class="logo_s img-fluid"> --}}
+                            </div>
+                            <div class="goleadores">
+                                <span class="d-block">{!! $goles_local !!}</span>
+                            </div>
+                        </div>
+
+                        <div class="marcador">
+                            <span class="goles-local color-red">
+
+                                <?php
+                                if ($partido->estado_partido !== 3) {
+                                    echo $partido->goles_local;
+                                } else {
+                                    if ($partido->goles_local == '0'):
+                                        echo '-';
+                                    else:
+                                        echo $partido->goles_local;
+                                    endif;
+                                }
+                                ?>
+                            </span>
+                            <span class="fs-01 color-red">-</span>
+                            <span class="goles-visitante color-red">
+                                <?php
+                                
+                                if ($partido->estado_partido !== 3) {
+                                    echo $partido->goles_visitante;
+                                } else {
+                                    if ($partido->goles_visitante == '0'):
+                                        echo '-';
+                                    else:
+                                        echo $partido->goles_visitante;
+                                    endif;
+                                }
+                                ?>
+                            </span>
+                        </div>
+
+                        <div class="visitante">
+                            <div class="escudo d-lg-inline-block d-none">
+                                {{-- <img src="{{ asset('assets/images/img/club/escudo' . $partido->equipoVisitante_id) }}.png" class="logo_s img-fluid"> --}}
+                            </div>
+                            <h3 class="d-block">{{ $partido->visitante }}</h3>
+                            <div class="goleadores">
+                                <span class="d-block">{!! $goles_visitante !!}</span>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            @endforeach
+
+        </div>
+        <?php } ?>
+
     </section>
 
     <script>
