e3674f8d6edd8c02f42841a39b1bf946d8725dd7
Michell Arenales <68234112+Mich-proart@users.noreply.github.com>
2024-01-21 16:07:45 +0100

    ajustes de ficha de equipo2

--- resources/views/fichaEquipo.blade.php
+++ resources/views/fichaEquipo.blade.php
@@ -119,9 +119,10 @@
                     <?php for ($i = 0; $i < 10; $i++) { ?>
                     <?php
                     /* $horabd = $partido->hora_prevista;
-                    $HoraLimpia = substr($horabd, 0, -3); */ // Elimina los tres últimos caracteres :00
+                    $HoraLimpia = substr($horabd, 0, -3); */
+                    // Elimina los tres últimos caracteres :00
                     ?>
-                    <div id="<?php echo $i + 1; ?>" class="PartidosFuturos bg-white p-lg-4 p-3 mb-2">
+                    <div id="" class="PartidosFuturos bg-white p-lg-4 p-3 mb-2">
                         <div class="d-flex aling-items-center justify-content-between partido_futuro">
                             <div class="col d-flex aling-items-center ">
                                 <div class="d-block py-2 px-1 fs-2">
@@ -133,72 +134,46 @@
                                 </div>
                                 <div>
                                     <span
-                                        class="mx-4 hora minitos_partidos d-inline-block fw-semibold px-3 bg-gray-hora text-white">{{ $HoraLimpia }}</span>
+                                        class="mx-4 hora minitos_partidos d-inline-block fw-semibold px-3 bg-gray-hora text-white">18:00</span>
                                 </div>
 
                             </div>
                             <div class="col">
                                 <span class="fs-00 d-grid grid-center-xy jornada_tiempo">Jornada
-                                    {{ $partido->jornada }}</span>
+                                    <?php echo $i + 1; ?></span>
                             </div>
                             <div
                                 class="col text-end mas_info_partido d-flex aling-items-center justify-content-end gap-4">
-                                {{-- <span class="icon-alienacion d-inline-block fs-1"></span>
-                                <span class="icon-ball d-inline-block fs-1"></span> --}}
+                                <span class="icon-alienacion d-inline-block fs-1"></span>
+                                <span class="icon-ball d-inline-block fs-1"></span>
                                 <span class="icon-TV d-inline-block fs-1"></span>
                             </div>
 
                         </div>
                         <div class="mx-0 partido_alineado my-2">
                             <div class="local">
-                                <h3 class="d-block">{{ $partido->local }}</h3>
+                                <h3 class="d-block">Local</h3>
                                 <div class="escudo d-lg-inline-block d-none">
                                     {{-- <img src="{{ asset('assets/images/img/club/escudo' . $partido->equipoLocal_id) }}.png" class="logo_s img-fluid"> --}}
                                 </div>
                                 <div class="goleadores">
-                                    <span class="d-block">{!! $goles_local !!}</span>
+                                    <span class="d-block">{{-- {!! $goles_local !!} --}}</span>
                                 </div>
                             </div>
 
                             <div class="marcador">
-                                <span class="goles-local color-red">
-
-                                    <?php
-                                    if ($partido->estado_partido !== 3) {
-                                        echo $partido->goles_local;
-                                    } else {
-                                        if ($partido->goles_local == '0'):
-                                            echo '-';
-                                        else:
-                                            echo $partido->goles_local;
-                                        endif;
-                                    }
-                                    ?>
-                                </span>
+                                <span class="goles-local color-red">0 </span>
                                 <span class="fs-01 color-red">-</span>
-                                <span class="goles-visitante color-red">
-                                    <?php
-                                    
-                                    if ($partido->estado_partido !== 3) {
-                                        echo $partido->goles_visitante;
-                                    } else {
-                                        if ($partido->goles_visitante == '0'):
-                                            echo '-';
-                                        else:
-                                            echo $partido->goles_visitante;
-                                        endif;
-                                    }
-                                    ?>
-                                </span>
+                                <span class="goles-visitante color-red"> 1 </span>
                             </div>
 
                             <div class="visitante">
                                 <div class="escudo d-lg-inline-block d-none">
                                     {{-- <img src="{{ asset('assets/images/img/club/escudo' . $partido->equipoVisitante_id) }}.png" class="logo_s img-fluid"> --}}
                                 </div>
-                                <h3 class="d-block">{{ $partido->visitante }}</h3>
+                                <h3 class="d-block">Visitante</h3>
                                 <div class="goleadores">
-                                    <span class="d-block">{!! $goles_visitante !!}</span>
+                                    <span class="d-block">{{-- {!! $goles_visitante !!} --}}</span>
                                 </div>
                             </div>
                         </div>
